0427abeb666d834e97c7bd1a8c3b1426966f9e59
Rohansha Shaik <irohansha@gmail.com>
2026-02-07 15:59:06 +0530

    rose day

---
+++ src/RoseDay.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import "./RoseDay.css";
+
+export default function RoseDay() {
+  const fullName = "@@@@@"; // 👉 change name here
+  const [text, setText] = useState("");
+  const [showMessage, setShowMessage] = useState(false);
+
+  // Typewriter effect
+  useEffect(() => {
+    let index = 0;
+    const interval = setInterval(() => {
+      setText(fullName.slice(0, index + 1));
+      index++;
+      if (index === fullName.length) clearInterval(interval);
+    }, 150);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <div className="hearts-bg"></div>
+
+      <div className="card">
+        <h1>Happy Rose Day 🌹</h1>
+
+        <h2 id="typewriter">{text}</h2>
+
+        <p>
+          A rose for your smile,
+          <br />
+          <br />
+          just for you ❤️
+        </p>
+
+        <button onClick={() => setShowMessage(true)}>
+          Open My Heart 💖
+        </button>
+
+        {showMessage && (
+          <div id="hiddenText">
+            You make ordinary moments feel special 🌸
+            <br />
+            Happy Rose Day ❤️
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
